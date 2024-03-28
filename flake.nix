8987b9f3f9a4bdbfb5e7c4c0a30527e21affb51e
SilentVoid13 <51264226+SilentVoid13@users.noreply.github.com>
2024-03-28 11:20:36 +0100

    build: adding flake.nix

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Silent_Pass";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs: let
+  in
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+        buildInputs = with pkgs; [
+          libmhash
+        ];
+        nativeBuildInputs = with pkgs; [
+          clang
+          pkg-config
+          cmake
+          nasm
+        ];
+      in {
+        devShells.default = pkgs.mkShell {
+          inherit buildInputs;
+          inherit nativeBuildInputs;
+        };
+      }
+    );
+}
